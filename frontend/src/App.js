1eeae82707934fac8e08025ed6333d16cedfa9a6
mitaksim1 <miriamt.simonnet@gmail.com>
2020-09-09 21:12:36 +0200

    Add background image and first css configuration WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import './App.css';
+
+// Imports dans l'application
+import logo from './Assets/coffee-logo2.png';
 
 function App() {
   return (
-  <h1>Hello World</h1>
+    <div className="container">
+      <img className="logo" src={logo} alt="Coffee&Code"/>
+
+      <div className="content">
+        <p>Réservez un <strong>Coffee Bar</strong> pour coder vos projets et rencontrez d'autres <strong>développeurs</strong> pour partager vos idées</p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+          type="email" 
+          id="email" 
+          placeholder="Votre e-mail" 
+          />
+          <Button variant="dark">Validez</Button>
+        </form>
+    </div>
+  </div>   
   );
 }
 
