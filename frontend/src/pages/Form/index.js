d90ea16360b6feb50bacff12db1794d301ae887a
mitaksim1 <miriamt.simonnet@gmail.com>
2020-09-10 16:54:13 +0200

    Getting user's email and password

--- frontend/src/pages/Form/index.js
+++ frontend/src/pages/Form/index.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
-// import api from '../../services/api';
+import api from '../../services/api';
 import './style.css';
 
 export default function Form() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    // Form submit method
     function handleSubmit(event) {
         event.preventDefault();
-        console.log('Hello');
+        console.log(email);
+        console.log(password);
       }
+
+      // Changes email's input value
+      function handleEmailChange(event) {
+          setEmail(event.target.value);
+      }
+
+      // Changes password's input value
+      function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
     return (
         <div className="content">
         <p>Réservez un <strong>Coffee Bar</strong> pour coder vos projets et rencontrez d'autres <strong>développeurs</strong> pour partager vos idées!</p>
@@ -20,12 +35,16 @@ export default function Form() {
           type="email" 
           id="email" 
           placeholder="Votre e-mail" 
+          value={ email }
+          onChange={handleEmailChange}
           />
           <label htmlFor="password">PASSWORD</label>
           <input 
           type="password" 
           id="password" 
           placeholder="Votre password" 
+          value={ password }
+          onChange={handlePasswordChange}
           />
           <Button type="submit" variant="dark">Validez</Button>
           {/*<a href="">Créez votre compte gratuitement pour commencer à profiter des événements Code & Coffee!</a>*/}
