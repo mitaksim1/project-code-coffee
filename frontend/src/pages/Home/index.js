9d854cc1a8a4a715c59f194d94561acf44cec23c
mitaksim1 <miriamt.simonnet@gmail.com>
2020-09-16 22:37:01 +0200

    Creating sign up page and redirection to home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
-import ButtonModal from '../ButtonModal';
-import FormModal from '../FormModal';
+import { Button } from 'react-bootstrap';
 import Header from '../Header';
-
 
 export default function Home() {
     return (
@@ -14,8 +13,9 @@ export default function Home() {
             <h1 className="title">Transformez <span>café en code!</span></h1>
             <p>Réservez un <strong>Coffee Bar</strong> pour coder vos projets et rencontrez d'autres <strong>développeurs</strong> pour partager vos idées!</p>
             <p>Créez votre événement Code&Coffee ou participez à un des événements déjà prévus près de chez vous sur simple inscription.</p>
-            <ButtonModal />
-            <FormModal />
+        <Link className="link_button" to="/signup">
+            <Button className="home_button" type="submit" variant="dark">S'inscrire!</Button>
+        </Link>
         </div>    
         </>
     );
---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Button } from 'react-bootstrap';
+
+import './style.css';
+import api from '../../services/api';
+import logo from '../../Assets/coffee-logo1.svg';
+
+export default function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Form submit method
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    // Requesting API
+    try {
+      const response = await api.post('/sessions', { name, email, password });
+      
+      console.log(response);
+
+    } catch(err) {
+      console.log('Erreur');
+
+    } 
+}
+
+  // Changes name's input value
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  // Changes email's input value
+  function handleEmailChange(event) {
+      setEmail(event.target.value);
+  }
+
+  // Changes password's input value
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+}
+
+
+  return (
+    <div className="signup_container">
+      <div className="signup_content">
+        <section>
+          <img className="signup_logo" src={logo} alt="Code&Coffee"/>
+          <h1>Inscription</h1>
+          <p className="text_signup">Après votre inscription, vous pourrez rencontrer d'autres professionnels dans un des nos événements Code&Coffee</p>
+         
+        </section>
+          <form className="content_form" autoComplete="off" onSubmit={handleSubmit}>
+          <label className="content_label" htmlFor="name">NOM</label>
+          <input className="input_element"
+          type="text" 
+          id="name" 
+          placeholder="Votre nom" 
+          required
+          value={ name }
+          onChange={handleNameChange}
+          />
+          <label className="content_label" htmlFor="email">E-MAIL</label>
+          <input className="input_element"
+          type="email" 
+          id="email" 
+          placeholder="Votre e-mail" 
+          required
+          value={ email }
+          onChange={handleEmailChange}
+          />
+          <label className="content_label" htmlFor="password">MOT DE PASSE</label>
+          <input className="input_element"
+          type="password" 
+          id="password" 
+          placeholder="Votre mot de passe"
+          required 
+          value={ password }
+          onChange={handlePasswordChange}
+          />
+          <Button
+            className="submit_button" 
+            type="submit" 
+            variant="dark">Validez
+          </Button>        
+          </form>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="black" />
+            Revenir à la page d'accueil
+          </Link>
+      </div>
+    </div>
+  )
+}
