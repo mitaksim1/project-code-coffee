a65414cf4292df86787d598556d30591b28e2079
mitaksim1 <miriamt.simonnet@gmail.com>
2020-09-22 23:56:10 +0200

    Call method virtual of mongoose to upload photos in frontend

--- backend/models/CoffeeEvent.js
+++ backend/models/CoffeeEvent.js
@@ -11,6 +11,15 @@ const CoffeeSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
+}, {
+    toJSON: {
+        virtual: true, // à chaque fois q'un spot est converti en json, il faut calculer les virtuals automatiquement
+    }
+});
+
+// Partie crée au moment d'ahouter les photos dans le dashboard frontend
+CoffeeSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files$(this.thumbnail)`
 });
 
 // Compiling schema into a model
